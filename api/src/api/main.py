1294ac6232117a181a273527756e7ec5a22bf0c9
sant <167131896+matheus3881@users.noreply.github.com>
2026-08-13 16:46:04 +0000

    remove comentários do main

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -14,16 +14,3 @@ def raiz():
 def status():
     return {"status": "OK", "version": "1.0"}
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "matheus"}
-
-# @app.get("/status")
-# def status():
-#     return{"status": "ok", 
-#            "version": "q.0" }
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: str | None = None):
-#     return {"item_id": item_id, "q": q}
